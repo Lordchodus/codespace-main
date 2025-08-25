b5357052baa08df6096957c58741e922343390a3
Lordchodus <narakusdemon@gmail.com>
2025-08-25 17:06:55 -0500

    practice notes

--- code.py
+++
@@ -1,6 +0,0 @@
-name = "cody"
-age = 35
-state = "KY"
-country = "USA"
-
-print(f"Hello, my name is {name}. I am {age} years old and I live in {state}, {country}.")  
---
+++ practice.py
@@ -0,0 +1,16 @@
+# print (3* "ab")
+# print ('3'+'ab')
+# print (str(3)+'ab')
+
+# a=1
+# print (a<3)
+
+
+# x=3*5
+# print (x)
+# print (x/5)
+
+# x=15
+# if (x/2)*2 == x:
+# 	print ('Even')
+# else: print ('Odd')
