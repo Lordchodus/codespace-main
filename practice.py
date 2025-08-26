6d62cd71f61415bf39afb0de18f49d1701b8881e
Lordchodus <narakusdemon@gmail.com>
2025-08-26 13:40:24 -0500

    test = (my, balls, really, hurts) test

--- practice.py
+++ practice.py
@@ -14,3 +14,21 @@
 # if (x/2)*2 == x:
 # 	print ('Even')
 # else: print ('Odd')
+
+# x=10
+# i=1
+# while i<=x:
+#   print (i)
+#   i=i+1
+
+# x=10
+# for i in range (1,x):
+#   if x%i==0:
+#     print ('divisor',i)
+
+# x=0
+# i=30
+# while i>=x:
+#   print (i)
+#   i=i-1
+
