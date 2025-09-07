1062df732292e4ac3991e55552991325881e518b
Lordchodus <narakusdemon@gmail.com>
2025-09-07 12:08:16 -0500

    added notes from the reddit link

--- notes.py
+++ notes.py
@@ -724,3 +724,39 @@ def checkout():
 total = checkout()
 assert total == 27.50, f"Expected 27.50 but got {total}"
 print("Total is:", total)
+
+# week one project for 
+# --------Receipt Calculator------- #
+def calc_total(price:float, quantity:int):
+    return price * quantity
+
+def calc_grand_total(item1:float, item2:float):
+    return item1 +  item2
+
+item1_name = input(f"enter item name: ")
+item1_price = float(input(f"enter price of {item1_name}: $"))
+item1_quantity = int(input(f"enter quantity of {item1_name}: "))
+
+item2_name = input(f"enter item name: ")
+item2_price = float(input(f"enter price of {item2_name}: $"))
+item2_quantity = int(input(f"enter quantity of {item2_name}: "))
+
+item1_total = calc_total(item1_price, item1_quantity)
+item2_total = calc_total(item2_price, item2_quantity)
+total = calc_grand_total(item1_total, item2_total) 
+
+print("\n----- RECEIPT -----")
+print(f"{item1_name} x{item1_quantity} @ ${item1_price:>2.2f} = ${item1_total:>2.2f}")
+print(f"{item2_name} x{item2_quantity} @ ${item2_price:>2.2f} = ${item2_total:>2.2f}")
+print(f"Total: ${total:>2.2f}")
+
+# if/else 
+if thirsty:
+    print("Get some water")
+else:
+    print("Keep working")
+    
+    # notes from the reddit 
+   # https://docs.google.com/document/d/1hvW-N_bS_OwE_IhmxJh1PMyyvHr5XVkODPU23qA8kJE/edit?usp=sharing
+
+
