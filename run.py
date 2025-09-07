2969644d4693cef019b3340372d58a48cef0d03e
Lordchodus <narakusdemon@gmail.com>
2025-09-07 12:06:49 -0500

    good progress this week homie. now get some rest

---
+++ run.py
@@ -0,0 +1,11 @@
+def usr_input(yes: str, no: str):
+    return yes 
+
+question = input(f"are you thirsty? ")
+
+answer = usr_input
+
+if usr_input:
+    print("Get some water")
+else:
+    print("Keep working")
