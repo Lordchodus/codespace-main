9369789ea2954004f5404484e3c63ad04e569dc5
Lordchodus <narakusdemon@gmail.com>
2025-09-07 10:58:18 -0500

    finished pray for me

--- weekoneproject.py
+++ weekoneproject.py
@@ -1,8 +1,9 @@
-def calc_total(price:float, quantity):
+# --------Receipt Calculator------- #
+def calc_total(price:float, quantity:int):
     return price * quantity
 
-def calc_grand_total(total1:float, total2:float):
-    return total1 + total2
+def calc_grand_total(item1:float, item2:float):
+    return item1 +  item2
 
 item1_name = input(f"enter item name: ")
 item1_price = float(input(f"enter price of {item1_name}: $"))
@@ -15,6 +16,11 @@ item2_quantity = int(input(f"enter quantity of {item2_name}: "))
 item1_total = calc_total(item1_price, item1_quantity)
 item2_total = calc_total(item2_price, item2_quantity)
 total = calc_grand_total(item1_total, item2_total) 
+
+print("\n----- RECEIPT -----")
+print(f"{item1_name} x{item1_quantity} @ ${item1_price:>2.2f} = ${item1_total:>2.2f}")
+print(f"{item2_name} x{item2_quantity} @ ${item2_price:>2.2f} = ${item2_total:>2.2f}")
+print(f"Total: ${total:>2.2f}")
 
 
 
