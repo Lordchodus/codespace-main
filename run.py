12dce16706afd9853eedd048bac14f4e28aa66b9
Lordchodus <narakusdemon@gmail.com>
2025-09-11 18:15:33 -0500

    continues

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-team1 = "liverpool"
-team2 = "chelsea"
-score1 = 4
-score2 = 3
-print(f"{team1} versus {team2} score {score1} to {score2}")
-print('they said "try it!" so i did')
-
-print(len("hi al!"))
+total = 0
+count = 0
+for num in [7, 4, 6]:
+    total += num
+    count += 1
+print(total, count)
