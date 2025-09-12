c695629fdc59e11daf1be1a8ad8e9a26e3504876
Lordchodus <narakusdemon@gmail.com>
2025-09-12 10:50:45 -0500

    adding to lists

--- run.py
+++ run.py
@@ -1,8 +1,30 @@
-smallest = None
-largest = None
-for num in [5, 3, 9]:
-    if smallest is None or num < smallest:
-        smallest = num
-    if largest is None or num > largest:
-        largest = num
-print(smallest, largest)
+fav_food = "chinese food", "pizza", "spaghetti"
+print(fav_food)
+
+
+fav_food = ["chinese food", "pizza", "spaghetti", "grilled chicken"]
+fav_food += ["steak and eggs"] # adding to a list
+print(len(fav_food)) # printing length of list
+print(fav_food)
+
+fav_food = ["chinese food", "pizza", "spaghetti", "grilled chicken"]
+fav_food += ["steak and eggs"] # adding to a list
+fav_food[2] = "lasagna" # repalacing item in a list 
+print(len(fav_food)) # printing length of list
+print(fav_food)
+print(fav_food[1]) # 0 start indexing = lists start at 0 not 1 
+print(fav_food[-1]) # prints last item in the list
+
+
+fav_food = ["chinese food", "pizza", "spaghetti", "grilled chicken"]
+fav_food += ["steak and eggs"] # adding to a list
+fav_food[2] = "lasagna" # repalacing item in a list 
+print(len(fav_food)) # printing length of list
+print(fav_food)
+print(fav_food[1])
+print(fav_food[-1])
+
+
+
+
+
