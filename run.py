10a26929d73b958d98a23c5bb009d95029f128d5
Lord Chodus <narakusdemon@gmail.com>
2025-09-11 23:14:35 -0500

    fiding small and large from list

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-total = 0
-count = 0
-for num in [7, 4, 6]:
-    total += num
-    count += 1
-print(total, count)
+smallest = None
+largest = None
+for num in [5, 3, 9]:
+    if smallest is None or num < smallest:
+        smallest = num
+    if largest is None or num > largest:
+        largest = num
+print(smallest, largest)
