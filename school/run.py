2b24b0b2743cf2b94ce89b724f150c64c3a33f48
Lordchodus <narakusdemon@gmail.com>
2025-09-30 14:17:49 -0500

    learning lambda

--- school/run.py
+++ school/run.py
@@ -1,25 +1,26 @@
 import random
 
-def d6_roll():
-    return random.randint(1, 6)
+rolls = {
+    'd6': lambda: random.randint(1, 6),
+    'd8': lambda: random.randint(1, 8),
+    'd10': lambda: random.randint(1, 10),
+    'd12': lambda: random.randint(1, 12),
+    'd20': lambda: random.randint(1, 20),
+}
 
-def d8_roll():
-    return random.randint(1, 8)
-
-def d10_roll():
-    return random.randint(1, 10)
 
 while True: 
     command = input("Enter command: ").lower().strip()
- 
-    if command in ['d6']:
-        result = d6_roll()  # Call once and store the result
+    if command in ['quit']:
+        break
+    elif command in ['d6']:
+        result = rolls['d6']()  # Call once and store the result
         print(f"you rolled a {result}")
     elif command in ['d8']:
-        result = d8_roll()
+        result = rolls['d8']()
         print(f"you rolled a {result}")
     elif command in ['d10']:
-        result = d10_roll()
-        print(f"you rolled a {result}")    
-
+        result = rolls['d10']()
+        print(f"you rolled a {result}")
     
+
